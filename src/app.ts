77b8af58088a3a9aa34d04145100049a5133809c
Redoxahmii <fullmeta2334@gmail.com>
2024-01-12 04:00:58 +0500

    init

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+
+import * as middlewares from "./middlewares";
+import api from "./api";
+import MessageResponse from "./interfaces/MessageResponse";
+
+require("dotenv").config();
+const app = express();
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get<{}, MessageResponse>("/", (_req, res) => {
+  res.json({
+    message: "hate life",
+  });
+});
+
+app.use("/api/", api);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+
+
+export default app;
